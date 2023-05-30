3d5c798ac598ac9fa0d8d724ad520aed949ab071
alphaura <kylemagri98@gmail.com>
2023-05-30 20:08:33 +0200

    Added the HTML, CSS and JS files

---
+++ script.js
@@ -0,0 +1,161 @@
+// JavaScript Document
+
+const welcome1 = document.getElementById("wel1");
+
+welcome1.addEventListener("click", () => {
+  const textToCopy = "Welcome to Amaz1ng Cas1no! Thank you for choosing our platform for your gaming needs. How can I assist you today?"; // Replace with the actual text you want to copy
+  
+  navigator.clipboard.writeText(textToCopy)
+    .then(() => {
+      console.log("Text copied to clipboard:", textToCopy);
+      // Optionally, you can show a success message to the user or perform any other actions
+    })
+    .catch((error) => {
+      console.error("Failed to copy text:", error);
+      // Handle any errors that occur during copying
+    });
+});
+
+const welcome2 = document.getElementById("wel2");
+
+welcome2.addEventListener("click", () => {
+  const textToCopy = "Hello and welcome to [Online Casino/Gambling Site]! It's great to have you here. If you have any questions or need any help, please don't hesitate to ask."; // Replace with the actual text you want to copy
+  
+ navigator.clipboard.writeText(textToCopy)
+    .then(() => {
+      console.log("Text copied to clipboard:", textToCopy);
+      // Optionally, you can show a success message to the user or perform any other actions
+    })
+    .catch((error) => {
+      console.error("Failed to copy text:", error);
+      // Handle any errors that occur during copying
+    });
+});
+
+const bonus1 = document.getElementById("bon1");
+
+bonus1.addEventListener("click", () => {
+  const textToCopy = "That's a great question. Our bonus offers are subject to terms and conditions, which you can find on our website. I recommend reviewing them carefully before claiming a bonus. If you need any help, just let me know"; // Replace with the actual text you want to copy
+  
+ navigator.clipboard.writeText(textToCopy)
+    .then(() => {
+      console.log("Text copied to clipboard:", textToCopy);
+      // Optionally, you can show a success message to the user or perform any other actions
+    })
+    .catch((error) => {
+      console.error("Failed to copy text:", error);
+      // Handle any errors that occur during copying
+    });
+});
+
+const bonus2 = document.getElementById("bon2");
+
+bonus2.addEventListener("click", () => {
+  const textToCopy = "Thank you for your interest in our bonus offers. To claim a bonus, please visit the promotions section of our website and follow the instructions provided. If you have any further questions, please let me know."; // Replace with the actual text you want to copy
+  
+ navigator.clipboard.writeText(textToCopy)
+    .then(() => {
+      console.log("Text copied to clipboard:", textToCopy);
+      // Optionally, you can show a success message to the user or perform any other actions
+    })
+    .catch((error) => {
+      console.error("Failed to copy text:", error);
+      // Handle any errors that occur during copying
+    });
+});
+
+const settled1 = document.getElementById("set1");
+
+settled1.addEventListener("click", () => {
+  const textToCopy = "I'm happy to inform you that your bet has been settled, and your winnings have been credited to your account. Thank you for placing your bet with us, and if you have any questions or feedback about your experience, please let us know. We're here to help!"; // Replace with the actual text you want to copy
+  
+ navigator.clipboard.writeText(textToCopy)
+    .then(() => {
+      console.log("Text copied to clipboard:", textToCopy);
+      // Optionally, you can show a success message to the user or perform any other actions
+    })
+    .catch((error) => {
+      console.error("Failed to copy text:", error);
+      // Handle any errors that occur during copying
+    });
+});
+
+const settled2 = document.getElementById("set2");
+
+settled2.addEventListener("click", () => {
+  const textToCopy = "Congratulations! Your bet has been settled, and your winnings have been credited to your account. If you have any further questions or concerns, please let us know. Thank you for choosing Amaz1ng Cas1no!"; // Replace with the actual text you want to copy
+  
+ navigator.clipboard.writeText(textToCopy)
+    .then(() => {
+      console.log("Text copied to clipboard:", textToCopy);
+      // Optionally, you can show a success message to the user or perform any other actions
+    })
+    .catch((error) => {
+      console.error("Failed to copy text:", error);
+      // Handle any errors that occur during copying
+    });
+});
+
+const denied1 = document.getElementById("den1");
+
+denied1.addEventListener("click", () => {
+  const textToCopy = "I'm sorry, but the bonus you're requesting is no longer available or not eligible for your account. Please check our promotions page for other available offers. If you have any questions, feel free to ask."; // Replace with the actual text you want to copy
+  
+ navigator.clipboard.writeText(textToCopy)
+    .then(() => {
+      console.log("Text copied to clipboard:", textToCopy);
+      // Optionally, you can show a success message to the user or perform any other actions
+    })
+    .catch((error) => {
+      console.error("Failed to copy text:", error);
+      // Handle any errors that occur during copying
+    });
+});
+
+const denied2 = document.getElementById("den2");
+
+denied2.addEventListener("click", () => {
+  const textToCopy = "I understand you're interested in the bonus, but unfortunately, you do not meet the requirements to claim it. However, we have other bonuses and promotions available that you may be eligible for. Please let us know if you're interested in exploring other options."; // Replace with the actual text you want to copy
+  
+ navigator.clipboard.writeText(textToCopy)
+    .then(() => {
+      console.log("Text copied to clipboard:", textToCopy);
+      // Optionally, you can show a success message to the user or perform any other actions
+    })
+    .catch((error) => {
+      console.error("Failed to copy text:", error);
+      // Handle any errors that occur during copying
+    });
+});
+
+const close1 = document.getElementById("clo1");
+
+close1.addEventListener("click", () => {
+  const textToCopy = "Thank you for chatting with us today. If you have any further questions, please don't hesitate to contact us again. Have a great day!"; // Replace with the actual text you want to copy
+  
+ navigator.clipboard.writeText(textToCopy)
+    .then(() => {
+      console.log("Text copied to clipboard:", textToCopy);
+      // Optionally, you can show a success message to the user or perform any other actions
+    })
+    .catch((error) => {
+      console.error("Failed to copy text:", error);
+      // Handle any errors that occur during copying
+    });
+});
+
+const close2 = document.getElementById("clo2");
+
+close2.addEventListener("click", () => {
+  const textToCopy = "Thank you for your time today. If you have any feedback about your experience with our customer service, we'd love to hear it. Have a great day and good luck in your gaming!"; // Replace with the actual text you want to copy
+  
+ navigator.clipboard.writeText(textToCopy)
+    .then(() => {
+      console.log("Text copied to clipboard:", textToCopy);
+      // Optionally, you can show a success message to the user or perform any other actions
+    })
+    .catch((error) => {
+      console.error("Failed to copy text:", error);
+      // Handle any errors that occur during copying
+    });
+});
